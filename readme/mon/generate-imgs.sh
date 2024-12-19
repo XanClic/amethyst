396003d614df1c53268c17272c4a99f2223dca81
Hanna Czenczek <hanna@xanclic.moe>
2024-12-19 21:12:28 +0100

    README: Do not use symlinks for images
    It doesn’t seem to work.

--- readme/mon/generate-imgs.sh
+++ readme/mon/generate-imgs.sh
@@ -41,6 +41,20 @@ for mon in \
     poltchageist \
     sinistcha
 do
+    rm -f "${mon}.gif"
     ../../tools/linear-to-gif.rb ../../gfx/pokemon/"$mon"/{front.png,anim.asm} "${mon}.gif"
+    rm -f "${mon}-shiny.gif"
     ../../tools/linear-to-gif.rb ../../gfx/pokemon/"$mon"/{shiny-front.png,anim.asm} "${mon}-shiny.gif"
+    cp ../../gfx/pokemon/"$mon"/back.png "${mon}-back.png"
+    if [ -f ../../gfx/pokemon/"$mon"/back-alt.png ]; then
+        cp ../../gfx/pokemon/"$mon"/back-alt.png "${mon}-alt-back.png"
+    fi
+    # Do not overwrite existing alt front images
+    # (Alt source files must always be 56x56, but we may want smaller cropped versions for the README.)
+    if [ -f ../../gfx/pokemon/"$mon"/front-alt.png -a ! -f "${mon}-alt.png" ]; then
+        cp ../../gfx/pokemon/"$mon"/front-alt.png "${mon}-alt.png"
+    fi
+    if [ -f ../../gfx/pokemon/"$mon"/shiny-front-alt.png -a ! -f "${mon}-alt-shiny.png" ]; then
+        cp ../../gfx/pokemon/"$mon"/shiny-front-alt.png "${mon}-alt-shiny.png"
+    fi
 done
